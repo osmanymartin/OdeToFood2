23959b7b0e056c30e579ca99158875c0d00558ca
OSMANY SAN MARTIN RUBI <osmany.giral@gmail.com>
2016-08-20 17:19:29 -0400

    test

--- OdeToFood2/App_Start/RouteConfig.cs
+++ OdeToFood2/App_Start/RouteConfig.cs
@@ -8,7 +8,7 @@ using System.Web.Routing;
 namespace OdeToFood2
 {
     public class RouteConfig
-    {
+    { 
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
